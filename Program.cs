c470dd7c36f42eaf82d2334bdbebcc5ee9a7be08
eduard <kuchmel.eded@mail.ru>
2022-07-15 15:24:56 +0300

    Added height

--- Program.cs
+++ Program.cs
@@ -10,11 +10,13 @@ namespace Menu
             string name = "";
             string surname = "";
             int age = 0;
+            int height = 0;
             while (response != -1)
             {
                 Console.WriteLine("Enter 0 to enter name" + Environment.NewLine +
                                     "Enter 1 to enter surname" + Environment.NewLine +
                                     "Enter 2 to enter age" + Environment.NewLine +
+                                    "Enter 3 to enter height" + Environment.NewLine +
                                     "Enter -2 to print your application" + Environment.NewLine +
                                     "Enter -1 to exit");
 
@@ -46,6 +48,11 @@ namespace Menu
                             Console.WriteLine(name);
                             Console.WriteLine(surname);
                             Console.WriteLine(age);
+                            break;
+                        }
+                    case 3:
+                        {
+                            height = int.Parse(Console.ReadLine());
                             break;
                         }
                     default:
